400bc6b042984b6eec6cc6174f85f6dbb5300dca
anaskhalil123 <aelshaikhkhalil@gmail.com>
2022-05-18 11:59:41 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:e_commerce_app/firebase_options.dart';
 import 'package:e_commerce_app/screens/admin/add_product.dart';
 import 'package:e_commerce_app/screens/admin/admin_home_screen.dart';
 import 'package:e_commerce_app/screens/admin/edit_product.dart';
-import 'package:e_commerce_app/screens/user/home_screen.dart';
 import 'package:e_commerce_app/screens/login_screen.dart';
 import 'package:e_commerce_app/screens/sigup_screen.dart';
+import 'package:e_commerce_app/screens/user/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
